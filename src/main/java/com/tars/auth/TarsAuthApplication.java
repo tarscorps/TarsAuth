ef2d5e9bd84d39ac0e444ad4a0296eee33c0184b
tarscorps <tarscorps>
2019-06-12 22:01:45 +0200

    init commit tarsAuth

---
+++ src/main/java/com/tars/auth/TarsAuthApplication.java
@@ -0,0 +1,13 @@
+package com.tars.auth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TarsAuthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TarsAuthApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tars/auth/TarsAuthApplicationTests.java
@@ -0,0 +1,16 @@
+package com.tars.auth;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class TarsAuthApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
